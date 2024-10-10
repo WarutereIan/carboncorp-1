d00215d809987a591c42c3d939d205425f5131f3
ian.dev <nmwanik111@gmail.com>
2024-10-10 18:00:46 +0300

    removed unused imports

--- src/utils/wagmiConfig.ts
+++ src/utils/wagmiConfig.ts
@@ -1,8 +1,8 @@
 import { http, createConfig } from "wagmi";
-import { mainnet, sepolia, liskSepolia, baseSepolia } from "wagmi/chains";
-import { metaMask, safe, walletConnect } from "wagmi/connectors";
+import { liskSepolia } from "wagmi/chains";
+import { metaMask } from "wagmi/connectors";
 
-const projectId = "f76be941c7ff7f66349322dc7b7a85f7";
+//const projectId = "f76be941c7ff7f66349322dc7b7a85f7";
 
 export const config = createConfig({
   //chains: [mainnet, sepolia, liskSepolia, baseSepolia],
